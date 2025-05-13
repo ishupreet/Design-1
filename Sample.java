b4396a16a166c6b7a9006e54f83c1848bef65a8e
Ishupreet Singh <isingh@rivian.com>
2025-05-13 16:58:08 -0700

    Design 1

--- Sample.java
+++ Sample.java
@@ -19,10 +19,42 @@ class MyHashSet {
   }
 }
 
+//time complexity = O(1)
+//space complexity = O(1)
+// take 2 stacks and one tracks the current stack value and other tracks the min seen till now
+class MinStack {
+
+  Stack<Integer> s, minStack;
+  public MinStack() {
+    s= new Stack<>();
+    minStack = new Stack<>();
+  }
+
+  public void push(int val) {
+    int min = s.isEmpty()?val:Math.min(val,minStack.peek());
+    s.add(val);
+    minStack.add(min);
+  }
+
+  public void pop() {
+    s.pop();
+    minStack.pop();
+  }
+
+  public int top() {
+    return s.peek();
+  }
+
+  public int getMin() {
+    return minStack.peek();
+  }
+}
+
 /**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
